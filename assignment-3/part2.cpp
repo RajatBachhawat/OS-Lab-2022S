c5fd08fc625f8090afa98c4df1f3475fb9c4157a
NehaDalmia <nehadalmia002@gmail.com>
2022-02-09 17:33:36 +0530

    p and c

--- assignment-3/part2.cpp
+++ assignment-3/part2.cpp
@@ -10,21 +10,46 @@
 #include<unistd.h>
 #include<string.h>
 #include <wait.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+pthread_mutex_t count_mutex;
 
 
 #define SHM_KEY 0x1234
+#define SEM_KEY 0x54321
 #define QUEUE_SZ 8
 #define MATRIX_DIM 2
 
 using namespace std;
 
+void P(sem_t *sem)
+{
+    if (sem_wait(sem) < 0)
+        perror("P error");
+}
+
+void V(sem_t *sem)
+{
+    if (sem_post(sem) < 0)
+        perror("V error");
+}
+
+
 struct computingJob 
 {
     int producerNo;
     int status;
-    int blocksDone;
-    double mat[MATRIX_DIM][MATRIX_DIM];
     int matid;
+    double mat[MATRIX_DIM][MATRIX_DIM];
+    computingJob(int pn, int s, int m):producerNo(pn),status(s),matid(m)
+    {
+        for(int i=0; i<MATRIX_DIM;i++)
+        {
+            for(int j=0; j<MATRIX_DIM;j++)
+                mat[i][j]=-9+rand()%19;
+        }
+    }
 };
 
 struct sharedMem     
@@ -32,19 +57,11 @@ struct sharedMem
     int jobCounter;
     int front;
     int back;
+    int count;
     computingJob cjQueue[QUEUE_SZ];
 };
 
 
-
-void initializeMatrix(double **&mat, int rows, int cols)
-{
-   mat = (double**)malloc(rows*(sizeof(double*)));
-   for(int i=0;i<rows;i++)
-   {
-      mat[i]=(double*)malloc(cols*sizeof(double));
-   }
-}
 
 void printMatrix(double** mat, int rows, int cols)
 {
@@ -60,13 +77,69 @@ void printMatrix(double** mat, int rows, int cols)
 
 int main(int argc, char *argv[]) 
 {
+   int NP,NW,MATS;
+   cout<<"Number of Producers: ";
+   cin>>NP;
+   cout<<"Number of Workers: ";
+   cin>>NW;
+   cout<<"Number of Matrices: ";
+   cin>>MATS;
+
    int shmid;
    sharedMem* shaddr;
 
    shmid = shmget(SHM_KEY, sizeof(sharedMem), IPC_CREAT|0600); 
    shaddr = (sharedMem*)shmat(shmid,NULL,0);
-   shaddr->front = shaddr->front = shaddr->jobCounter = 0;
+   shaddr->front =0;
+   shaddr->back = 0;
+   shaddr->jobCounter = 0;
+   shaddr->count =0;
+   //sem_init(&shaddr->mutex,0,0);
 
+   for(int i=0; i<NP;i++)
+   {
+       if(fork()==0)
+       {
+           while(true)
+           {
+               
+               if(shaddr->jobCounter>=MATS)
+               {
+                   cout<<"exit\n";
+                   exit(0);
+               }
+               while(shaddr->count == QUEUE_SZ);
+              // P(&shaddr->mutex);
+               computingJob cj(i,0,shaddr->jobCounter);
+               cout<<"Added: "<<cj.matid<<" "<<shaddr->count<<"\n";
+               shaddr->jobCounter+=1;
+               shaddr->cjQueue[shaddr->back] = cj;
+               shaddr->back = (shaddr->back+1)%QUEUE_SZ;
+               shaddr->count+=1;
+               //V(&shaddr->mutex);
+           }
+       }
+   }
+   for(int i=0; i<NW;i++)
+   {
+       if(fork()==0)
+       {
+           while(true)
+           {
+               while(shaddr->count == 0);
+              // P(&shaddr->mutex);
+               computingJob cj = shaddr->cjQueue[shaddr->front];
+               shaddr->front = (shaddr->front+1)%QUEUE_SZ;
+               cout<<"Matrix: "<<cj.matid<<" "<<shaddr->count<<"\n";
+               shaddr->count-=1;
+              // V(&shaddr->mutex);
+           }
+           
+       }
+   }
+   sleep(100);
+   shmdt(shaddr);
+   shmctl(shmid, IPC_RMID, 0);
 //    for(int i =0; i<QUEUE_SZ; i++)
 //    {
 //        shaddr->cjQueue[i].matid = i;
